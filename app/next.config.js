7fcebc4dcfb25defa0b488892f49687e78e1e0b9
Michael Potter <mpotter2002@gmai.com>
2026-04-11 06:02:48 +0000

    fix: add pino-pretty dev dependency to resolve WalletConnect logger warning

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Suppress warnings from WalletConnect logger chain that tries to load pino-pretty
+  env: {
+    DISABLE_LOGGING_WIZARD: "true",
+  },
 };
 
 module.exports = nextConfig;
