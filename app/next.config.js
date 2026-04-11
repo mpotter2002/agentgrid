74d77a97b96e2647920f16b8c050b73b295a336d
Michael Potter <mpotter2002@gmai.com>
2026-04-11 06:22:20 +0000

    revert: remove webpack alias from next.config (pino-pretty now installed as dep)

--- app/next.config.js
+++ app/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // Tell webpack/pino to treat missing optional modules as warnings, not errors
-    // This suppresses pino-pretty missing warnings from WalletConnect logger
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "pino-pretty": false,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
