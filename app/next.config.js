3ce48aa74f154def456adfbe66c8c30922f16b17
Michael Potter <mpotter2002@gmai.com>
2026-04-11 06:11:21 +0000

    fix: webpack alias pino-pretty to false to suppress missing optional module errors, add pino-pretty dependency

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Suppress warnings from WalletConnect logger chain that tries to load pino-pretty
-  env: {
-    DISABLE_LOGGING_WIZARD: "true",
+  webpack: (config) => {
+    // Tell webpack/pino to treat missing optional modules as warnings, not errors
+    // This suppresses pino-pretty missing warnings from WalletConnect logger
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "pino-pretty": false,
+    };
+    return config;
   },
 };
 
